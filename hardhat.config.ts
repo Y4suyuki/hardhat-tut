1d82e505fe1a7bbe51f0e7d1673a959d62110ef6
Yasuyuki Ageishi <y4suyuki@protonmail.com>
2023-11-09 23:22:58 +0900

    Add dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config'
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.METAMASK_SECRET ?? ""],
+    },
+  },
 };
 
 export default config;
